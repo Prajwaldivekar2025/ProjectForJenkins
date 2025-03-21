9a6b55667af9600d7bef2eac67b491657d1ebd59
Prajwaldivekar2025 <prajwaldivekar2032@gmail.com>
2025-03-21 15:16:29 +0530

    Initial commit

---
+++ src/test/java/mypackage/Program.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class Program {
+
+	public void Display() {
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Add="+c);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Program p =new Program();
+		p.Display();
+	}
+
+}
